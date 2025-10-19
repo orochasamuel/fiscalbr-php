0548a0d5372f380378543e4248a0a1939c887786
orochasamuel <samuelro.net@gmail.com>
2025-10-19 01:05:09 -0300

    correção nos tamanhos de campo de valor no Registro C190 da EFD Fiscal

--- src/EFDFiscal/BlocoC/RegistroC190.php
+++ src/EFDFiscal/BlocoC/RegistroC190.php
@@ -29,48 +29,48 @@ class RegistroC190 extends RegistroSped
     ///     correspondente ao somatório do valor das mercadorias, despesas acessórias
     ///     (frete, seguros e outras despesas acessórias), ICMS_ST e IPI.
 
-    #[SpedCampos(5, "VL_OPR", "N", 0, 2, true, 2)]
+    #[SpedCampos(5, "VL_OPR", "N", 15, 2, true, 2)]
     public Decimal $VlOpr;
 
 
     ///     Parcela correspondente ao "Valor da base de cálculo do ICMS" referente à
     ///     combinação de CST_ICMS, CFOP e alíquota do ICMS.
 
-    #[SpedCampos(6, "VL_BC_ICMS", "N", 0, 2, true, 2)]
+    #[SpedCampos(6, "VL_BC_ICMS", "N", 15, 2, true, 2)]
     public Decimal $VlBcIcms;
 
 
     ///     Parcela correspondente ao "Valor do ICMS" referente à combinação de CST_ICMS,
     ///     CFOP e alíquota do ICMS.
 
-    #[SpedCampos(7, "VL_ICMS", "N", 0, 2, true, 2)]
+    #[SpedCampos(7, "VL_ICMS", "N", 15, 2, true, 2)]
     public Decimal $VlIcms;
 
 
     ///     Parcela correspondente ao "Valor da base de cálculo do ICMS" da substituição
     ///     tributária referente à combinação de CST_ICMS, CFOP e alíquota do ICMS.
 
-    #[SpedCampos(8, "VL_BC_ICMS_ST", "N", 0, 2, true, 2)]
+    #[SpedCampos(8, "VL_BC_ICMS_ST", "N", 15, 2, true, 2)]
     public Decimal $VlBcIcmsSt;
 
 
     ///     Parcela correspondente ao valor creditado/debitado do ICMS da substituição
     ///     tributária, referente à combinação de CST_ICMS, CFOP e alíquota do ICMS.
 
-    #[SpedCampos(9, "VL_ICMS_ST", "N", 0, 2, true, 2)]
+    #[SpedCampos(9, "VL_ICMS_ST", "N", 15, 2, true, 2)]
     public Decimal $VlIcmsSt;
 
 
     ///     Valor não tributado em função da redução da base de cálculo do ICMS, referente
     ///     à combinação de CST_ICMS, CFOP e alíquota do ICMS.
 
-    #[SpedCampos(10, "VL_RED_BC", "N", 0, 2, true, 2)]
+    #[SpedCampos(10, "VL_RED_BC", "N", 15, 2, true, 2)]
     public Decimal $VlRedBc;
 
 
     ///     Parcela correspondente ao "Valor do IPI" referente à combinação CST_ICMS, CFOP e alíquota do ICMS.
 
-    #[SpedCampos(11, "VL_IPI", "N", 0, 2, true, 2)]
+    #[SpedCampos(11, "VL_IPI", "N", 15, 2, true, 2)]
     public Decimal $VlIpi;
 
 
